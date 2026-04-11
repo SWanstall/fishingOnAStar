e68b2eb1c93660d136af0fe2703720e5f5e6dfea
SWanstall <s.wanstall.13@gmail.com>
2026-04-11 11:39:34 +0100

    Initial commit

---
+++ scripts/character.gd
@@ -0,0 +1,87 @@
+extends CharacterBody2D
+
+@onready var bobbing_timer = $BobbingTimer
+@onready var set_hook_timer = $SetHookTimer
+@onready var fishing_ = $"Fishing!"
+@onready var hooked_ = $"Hooked!"
+@onready var bite_ = $"Bite!"
+
+var can_set_hook = false
+var hooked = false
+var bobbing = false
+
+const SPEED = 5000.0
+const JUMP_VELOCITY = -400.0
+
+func _ready():
+	fishing_.visible = false
+	bite_.visible = false
+	hooked_.visible = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_up") and can_set_hook == true and bobbing == true:
+		print("HOOKED!")
+		hooked = true
+		hooked_.visible = true
+		bobbing = false
+		fishing_.visible = false
+		can_set_hook = false
+		bite_.visible = false
+		await get_tree().create_timer(5).timeout
+		hooked_.visible = false
+		hooked = false
+
+func _physics_process(delta):
+	 #Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	#var erection = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.x = direction * SPEED * delta
+	#elif erection:
+		#velocity.y = erection * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_fishing_zone_fishing():
+	if bobbing == false and hooked == false:
+		var bobbing_time = randf_range(2.0, 5.0)
+		bobbing = true
+		fishing_.visible = true
+		bobbing_timer.start(bobbing_time)
+
+
+func _on_bobbing_timer_timeout():
+	print(bobbing)
+	print("bobbing timeout----------------------")
+	if hooked == false and bobbing == true:
+		bite_.visible = true
+		fishing_.visible = false
+		print("set hook!")
+		set_hook_timer.start(2)
+		can_set_hook = true
+
+
+
+func _on_set_hook_timer_timeout():
+	print("set hook timeout----------------------")
+	print("hooked = %s" % hooked)
+	#print()
+	if hooked == false and bobbing == true and can_set_hook == true:
+		can_set_hook = false
+		bite_.visible = false
+		print("Dang, just a nibble...")
+		bobbing = false
+		fishing_.visible = false
---
+++ scripts/fishing_zone.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var rod_in_range = false
+
+signal fishing
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if rod_in_range == true:
+		if Input.is_action_just_pressed("ui_up"):
+			print("FISHING! signal sent")
+			emit_signal("fishing")
+
+
+func _on_body_entered(body):
+	print("Press F for fishing!")
+	rod_in_range = true
+
+
+func _on_body_exited(body):
+	print("No fishing for you!")
+	rod_in_range = false
