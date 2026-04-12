7583257a40f2053d831739abc90f3648f35c3f99
SWanstall <s.wanstall.13@gmail.com>
2026-04-12 16:51:15 +0100

    Added scroll wheel reeling and fish tugging

--- scripts/character.gd
+++ scripts/character.gd
@@ -11,11 +11,17 @@ signal fish_processed
 @onready var hooked_ = $"Hooked!"
 @onready var bite_ = $"Bite!"
 @onready var animation_player = $AnimationPlayer
+@onready var progress_bar = $ProgressBar
+@onready var tug_timer = $TugTimer
 
 var can_set_hook = false
 var hooked = false
 var bobbing = false
 var processed = true
+var tugging = false
+
+var reeling_distance = 20
+var reeling_progress = 0
 
 const SPEED = 5000.0
 const JUMP_VELOCITY = -200
@@ -28,6 +34,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("lmb") and can_set_hook == true and bobbing == true:
 		print("HOOKED!")
+		progress_bar.visible = true
 		hooked = true
 		hooked_.visible = true
 		bobbing = false
@@ -35,15 +42,30 @@ func _process(delta):
 		can_set_hook = false
 		bite_.visible = false
 		processed = false
-		emit_signal("fish_landed")
 		await get_tree().create_timer(1.5).timeout
 		hooked_.visible = false
-		hooked = false
+		#hooked = false
 		
 	if Input.is_action_just_pressed("rmb"):
 		processed = true
 		print("processed = %s" % processed)
 		emit_signal("fish_processed")
+		
+	if Input.is_action_just_pressed("scroll"):
+		#print("reeling...")
+		if hooked == true:
+			tug_of_war()
+			reeling_progress = reeling_progress + 1.0
+			var reeling_percentage = (reeling_progress/reeling_distance)*100
+			progress_bar.set_value_no_signal(reeling_percentage)
+			if reeling_progress >= reeling_distance:
+				emit_signal("fish_landed")
+				reeling_progress = 0
+				reeling_percentage = (reeling_progress/reeling_distance)*100
+				progress_bar.set_value_no_signal(reeling_percentage)
+				hooked = false
+				progress_bar.visible = false
+
 
 func _physics_process(delta):
 	 #Add the gravity.
@@ -53,9 +75,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	if Input.is_action_just_pressed("scroll"):
-		print("reeling...")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -106,3 +125,21 @@ func _on_set_hook_timer_timeout():
 		print("Dang, just a nibble...")
 		bobbing = false
 		fishing_.visible = false
+
+
+func tug_of_war():
+	#print("tug of war!!!!!!!")
+	if rarity_value >= 80 and hooked == true and tugging == false:
+		var tugging_time = randf_range(0.2, 0.8)
+		tug_timer.start(tugging_time)
+		tugging = true
+
+
+func _on_tug_timer_timeout():
+	var tug_value = randf_range(1, 8)
+	reeling_progress = reeling_progress - tug_value
+	if reeling_progress < 0:
+		reeling_progress = 0
+	var reeling_percentage = (reeling_progress/reeling_distance)*100
+	progress_bar.set_value_no_signal(reeling_percentage)
+	
