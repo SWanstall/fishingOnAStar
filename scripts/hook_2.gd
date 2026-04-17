dcb776e851a33e0097aa906c1e8abf13cea073fb
SWanstall <s.wanstall.13@gmail.com>
2026-04-17 20:38:13 +0100

    Got hook2 spawning with velocity

---
+++ scripts/hook_2.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+var speed = 300
+
+#func get_input():
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#velocity = input_dir * speed
+
+func _physics_process(delta):
+	#get_input()
+	move_and_slide()
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
--- scripts/rod.gd
+++ scripts/rod.gd
@@ -2,9 +2,12 @@ extends Sprite2D
 
 var mouse_position = null
 
-@onready var character_body_2d = $CharacterBody2D
+@onready var rod = $Rod
 
 @export var hook_scene: PackedScene
+
+var hook2 = preload("res://scenes/hook_2.tscn")
+var speed = 200
 
 var prev_pos: Vector2
 var velocity: Vector2
@@ -20,15 +23,33 @@ func _process(delta):
 	look_at(mouse_position)
 
 func _physics_process(delta):
-	var current_pos = character_body_2d.global_position
+	var current_pos = rod.global_position
 	velocity = (current_pos - prev_pos) / delta
 	prev_pos = current_pos
 	
 	if Input.is_action_just_pressed("rmb"):
 		print(velocity)
-		var hook = hook_scene.instantiate()
+		#var hook = hook_scene.instantiate()
+#
+		#var rod_tip_position = character_body_2d.global_position
+#
+		## Spawn the mob by adding it to the Main scene.
+		#add_child(hook)
+	
+	if Input.is_action_just_pressed("rmb"):
+		shoot()
 
-		var rod_tip_position = character_body_2d.global_position
+#
+#func start(pos):
+	#position = pos
+	#show()
+	#$CollisionShape2D.disabled = false
 
-		# Spawn the mob by adding it to the Main scene.
-		add_child(hook)
+
+func shoot():
+	# "Muzzle" is a Marker2D placed at the barrel of the gun.
+	var current_pos = rod.global_position
+	var b = hook2.instantiate()
+	b.global_position = current_pos
+	b.velocity = velocity
+	get_tree().root.add_child(b)
