c19e963b87c2a49133ac33e12df757698fb6dd2f
SWanstall <s.wanstall.13@gmail.com>
2026-05-02 21:27:36 +0100

    Fixed resource to work with file system

---
+++ scripts/fish_stats.gd
@@ -0,0 +1,13 @@
+class_name fish_stats
+extends Resource
+
+@export var name: String
+
+@export var texture: Texture2D
+
+@export var max_size := 10
+@export var min_size := 1
+
+@export var coin_value := 1
+
+@export var pulling_power := 0
